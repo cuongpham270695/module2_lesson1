9e9740e0ffe267f58c208b6f07bca69a75cebc15
cuongpham270695 <79822838+cuongpham270695@users.noreply.github.com>
2021-04-04 09:52:27 +0700

    Add files via upload

---
+++ futurevalue.php
@@ -0,0 +1,61 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+    $data = [];
+    foreach ($_POST as $key => $value) {
+        if (!is_numeric($value)) {
+            $errors[] = "Please fill $key";
+            continue;
+        }
+        $data[$key] = floatval($value);
+    }
+
+    if (empty($errors)) {
+        $result = $data['investment'] * pow((1 + $data['interest']/100), $data['year']);
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>[Bài tập] Ứng dụng Future Value Calculator</title>
+    <style>
+        form > * {
+            margin: 20px 0;
+        }
+    </style>
+</head>
+<body>
+<h1>[Bài tập] Ứng dụng Future Value Calculator</h1>
+<form action="" method="post">
+    <div>
+        <input type="number" name="investment" id="investment" value="<?= $data['investment'] ?>">
+        <label for="investment">Investment Amount ($)</label>
+    </div>
+    <div>
+        <input type="number" name="interest" id="interest" value="<?= $data['interest'] ?>">
+        <label for="interest">Yearly Interest Rate (%)</label>
+    </div>
+    <div>
+        <input type="number" name="year" id="year" value="<?= $data['year'] ?>">
+        <label for="year">Number of Years</label>
+    </div>
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
+
+<?php if (!empty($errors)) : ?>
+    <?php foreach($errors as $value) : ?>
+        <h4><?= $value ?></h4>
+    <?php endforeach; ?>
+<?php endif; ?>
+
+<h4><?= $result ?? '' ?></h4>
+</body>
+</html>
